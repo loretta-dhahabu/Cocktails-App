c2d2b3f4df4e39314f093186b8592f51ed1b960c
loretta-dhahabu <loreydjefwa@gmail.com>
2022-07-25 00:12:34 +0300

    add error message

--- src/components/Error.js
+++ src/components/Error.js
@@ -1,10 +1,17 @@
 import React from 'react'
+import Button from "react-bootstrap/Button";
+import {Link} from "react-router-dom"
 
 function Error(){
   return (
-    <div>
-      <h1>error page</h1>
-    </div>
-  )
+    <section className="error-section">
+      <div className="error-container">
+        <h1>Ooops! You are caught up!</h1>
+        <Link to= "/" className="btn-home">
+          <Button variant="success">Back Home</Button>{" "}
+        </Link>
+      </div>
+    </section>
+  );
 }
 export default Error;

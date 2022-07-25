fd245eec5901b77034ce90a08657ef6db40eafaa
loretta-dhahabu <loreydjefwa@gmail.com>
2022-07-25 15:42:53 +0300

    display an array of cocktails

--- src/components/AppContext.js
+++ src/components/AppContext.js
@@ -8,7 +8,27 @@ const AppContext = React.createContext();
 function AppProvider({ children }) {
   const [loading, setLoading] = useState(true);
   const [searchName, setSearchName] = useState("a");
-  const [cocktails, setCocktails] = useState([]);
+  const [ cocktails, setCocktails ] = useState( [] );
+  
+   
+    useEffect( () =>
+    {
+      fetch( `${url}${searchName}` )
+        .then( response => response.json() )
+        .then( ( data ) =>console.log(data))
+        
+          setLoading( loading );
+          setCocktails(cocktails) 
+        
+      
+    }, [] )
+    
+    // if (data) {
+    //   const newCocktails = data.map((item) => {
+    //     const { idDrink, strDrink, strDRinkThumb, strAlcoholic, strGlass } = item;
+    //   });
+    // }
+
   return (
     <AppContext.Provider
       value={{ loading, searchName, cocktails, setSearchName }}
--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import images from "../images/gif1.gif";
 
 function Loading () {
   return (
     <div>
-      <h1>loading ...</h1>
+      <img src={images}/>
     </div>
   );
 }

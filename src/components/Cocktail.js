fffe19a7622061dc55ed10a141c32ae7d26bc7e8
loretta-dhahabu <loreydjefwa@gmail.com>
2022-07-25 21:13:45 +0300

    add cocktails details

--- src/components/Cocktail.js
+++ src/components/Cocktail.js
@@ -1,8 +1,18 @@
 import React from 'react'
+import {Link} from "react-router-dom"
 
-function Cocktail () {
+function Cocktail ({id,name,category,image,info,glass,instructions}) {
   return (
-    <div>Cocktail Component</div>
-  )
+    <article>
+      <div className='img-container'>
+        <img src={ image } alt="{name}" />
+      </div>
+      <div className='cocktail-footer'>
+        <h3>{ name }</h3>
+        <h4>{ glass }</h4>
+        <p>{ info }</p>
+      </div>
+    </article>
+  );
 }
 export default Cocktail;

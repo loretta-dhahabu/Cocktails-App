bb5699e1f000f63a4d326e23f76f54c8a6653724
loretta-dhahabu <loreydjefwa@gmail.com>
2022-07-26 23:26:37 +0300

    add a key to cocktails list

--- src/components/CocktailList.js
+++ src/components/CocktailList.js
@@ -1,39 +1,31 @@
-import React from 'react'
-import { useGlobalContext } from "../components/AppContext"
-import Loading from "./Loading"
-import Cocktail from './Cocktail'
+import React from "react";
+import { useGlobalContext } from "../components/AppContext";
+import Loading from "./Loading";
+import Cocktail from "./Cocktail";
 
-function CocktailList ()
-{
-    const { cocktails, loading } = useGlobalContext();
-    // console.log(cocktails);
+function CocktailList() {
+  const { cocktails, loading } = useGlobalContext();
+  // console.log(cocktails);
 
-    if ( loading )
-    {
-        return <Loading/> 
-    }
-    if ( cocktails.length < 1 )
-    {
-        return (
-            <h2 className='section-title'>
-                no cocktails matched your search criteria
-            </h2>
-        )
-    }
+  if (loading) {
+    return <Loading />;
+  }
+  if (cocktails.length < 1) {
+    return (
+      <h2 className="section-title">
+        no cocktails matched your search criteria
+      </h2>
+    );
+  }
   return (
-      <section className='section'>     
-          <h2 className='section-title'>Cocktail List Here</h2>
-          
-          <div className='cocktais-center'>
-              { cocktails.map( ( item ) =>
-              {
-                  return (
-                      <>
-                          <Cocktail key={ item.id } {...item}/>
-                      </>
-                  )
-              })}
-          </div>
+    <section className="section">
+      <h2 className="section-title">Cocktail List Here</h2>
+
+      <div className="cocktais-center">
+        {cocktails.map((item, id) => {
+          return <Cocktail key={id} {...item} />;
+        })}
+      </div>
     </section>
   );
 }

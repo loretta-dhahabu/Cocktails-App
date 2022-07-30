81a3b2fbfd50f859d0b387e1babdb739f1f82fbe
loretta-dhahabu <loreydjefwa@gmail.com>
2022-07-30 20:41:22 +0300

    remove toggle button

--- src/App.js
+++ src/App.js
@@ -9,38 +9,13 @@ import SingleCocktail from "./components/SingleCocktail";
 
 
 // import { AppContext, AppProvider } from "./components/AppContext";
-const getStorageTheme = () => {
-  let theme = "light-theme";
-  if (localStorage.getItem("theme")) {
-    theme = localStorage.getItem("theme");
-  }
-  return theme;
-};
-
 
 function App ()
 {
-  const [theme, setTheme] = useState(getStorageTheme());
-
-  const toggleTheme = () => {
-    if (theme === "light-theme") {
-      setTheme("dark-theme");
-    } else {
-      setTheme("light-theme");
-    }
-  };
-
-  useEffect(() => {
-    document.documentElement.className = theme;
-    localStorage.setItem("theme", theme);
-  }, [theme]);
   return (
     <div>
       <Router>
         <Navbar />
-        <button className="btn" onClick={toggleTheme}>
-          toggle
-        </button>
         <Switch>
           <Route exact path="/">
             <Home />
